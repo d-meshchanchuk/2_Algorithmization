50e8b9f39f92b1723986faa4a9e9a70dce02aaba
Dmitry <d.meshchanchuk@gmail.com>
2021-10-31 21:39:02 +0300

    add task09-10

--- src/OneDimensionalArrays/Task09.java
+++ src/OneDimensionalArrays/Task09.java
@@ -1,8 +1,37 @@
 package OneDimensionalArrays;
 
+/*
+    В массиве целых чисел с количеством элементов n найти наиболее часто встречающееся число.
+    Если таких чисел несколько, то определить наименьшее из них.
+*/
+
 public class Task09 {
 
     public static void main(String[] args) {
-        //TODO
+        int[] array = {1, 2,  2, 4, 5, 6, 7, 8, 2, 3, 3, 3, 3};
+        int[] arrayOfRepeatsNum = new int[array.length];
+        int minValue = array[0];
+        int counter = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    arrayOfRepeatsNum[i]++;
+                }
+            }
+
+            if (arrayOfRepeatsNum[i] > counter) {
+                counter = arrayOfRepeatsNum[i];
+                minValue = array[i];
+            }
+
+            if (arrayOfRepeatsNum[i] == counter && array[i] < minValue) {
+                counter = arrayOfRepeatsNum[i];
+                minValue = array[i];
+            }
+        }
+
+        System.out.println("Наиболее часто встречающееся число " + minValue);
+
     }
 }
---
+++ src/OneDimensionalArrays/Task10.java
@@ -0,0 +1,36 @@
+package OneDimensionalArrays;
+
+import java.util.Arrays;
+
+/*
+    Дан целочисленный массив с количеством элементов n.
+    Сжать массив, выбросив из него каждый второй элемент
+    (освободившиеся элементы заполнить нулями).
+    Примичание. Дополнительный массив при этом не использовать.
+*/
+
+public class Task10 {
+
+    public static void main(String[] args) {
+        int[] array = {1,2};
+        int count = 1;
+
+        System.out.println("Исходный массив " + Arrays.toString(array));
+
+        for (int i = 1; i < array.length; i++) {
+
+
+            if (i % 2 == 0) {
+                array[count] = array[i];
+                array[i] = 0;
+                count++;
+            } else {
+                array[i] = 0;
+
+            }
+        }
+
+        System.out.println("Конечный массив " + Arrays.toString(array));
+    }
+
+}
